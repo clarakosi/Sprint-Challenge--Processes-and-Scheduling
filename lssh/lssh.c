7a82d19b05ff1e1a3f2b119677a5a5b94ea41de6
Clara Andrew <candrewwani@gmail.com>
2018-04-13 13:19:31 -0400

    background tasks attempt

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -101,26 +101,38 @@ int main(void)
         #endif
         
         /* Add your code for implementing the shell's logic here */
+        int backgroundProgram = 0;
+
+        if (strcmp(args[0], "cd") == 0) {
+            if (args_count == 2) {
+                int dir = chdir(args[1]);
+                continue;
+                if(dir == -1) {
+                    perror("chdir");
+                }
+            } 
+            printf("Must include a directory after cd\n");
+        }
+
+        if (strcmp(args[args_count-1], "&") == 0) {
+            args[args_count-1] = NULL;
+            backgroundProgram = 1;
+            // continue;
+        }
+
         int rc = fork();
 
         if (rc < 0) {
             fprintf(stderr, "fork failed\n");
             exit(1);
         } else if (rc == 0) {
-            if (strcmp(args[0], "cd") == 0) {
-                if (args_count == 2) {
-                    printf("argument 1: %s\n", args[0]);
-                    int dir = chdir(args[1]);
-                    continue;
-                    if(dir == -1) {
-                        perror("chdir");
-                    }
-                } 
-                printf("Must include a directory after cd\n");
-            } 
             execvp(args[0], args);
         } else {
-            int wc = waitpid(rc, NULL, 0);
+            if (backgroundProgram) {
+                while (waitpid(-1, NULL, WNOHANG) > 0);
+            } else {
+                int wc = waitpid(rc, NULL, 0);
+            }
         }
         
     }
